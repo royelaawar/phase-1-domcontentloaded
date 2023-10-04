583c21d3be53c768f3a68aabc87402d2146ccaf8
Roy Elaawar <roy.elaawar@gmail.com>
2023-10-04 13:01:52 -0400

    DOM Content Loaded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const paragraphElement = document.getElementById('text')
+    // console.log(paragraphElement)
+    paragraphElement.textContent = "This is really cool!"
+    // console.log(paragraphElement)
+  });
+
